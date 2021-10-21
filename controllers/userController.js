8f49cc8d34f4abc835f6d7c59553534e57958d6e
raihanr1 <raihanrobbani123@gmail.com>
2021-10-21 15:53:19 +0700

    prorgress

---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+const { User } = require("../models")
+const { Op } = require("sequelize");
+const { isValidAccount } = require("../helper/helper")
+
+class userController {
+    static registerForm(req, res) {
+        let error;
+        if (req.query.error) {
+            error = req.query.error.split(',')
+        }
+        res.render('registerForm', {error})
+    }
+
+    static postRegisterForm(req, res) {
+        let {username, email, password, dateOfBirth, role} = req.body
+        User.create({username, email, password, dateOfBirth, role})
+        .then(newUser => {
+            res.redirect('/login')
+        })
+        .catch(err => {
+            let errors = err.errors.map(el => {
+                return el.message
+            })
+            res.redirect(`/register?error=${errors}`)
+        })
+    }
+
+    static loginForm(req, res) {
+        let error = req.query.error
+        res.render('login', {error})
+    }
+
+    static postLogin(req, res) {
+        let {usernameOremail, password} = req.body
+        User.findAll({
+            where:{
+                [Op.or]: [{
+                    username: usernameOremail
+                }, {
+                    email: usernameOremail
+                }]
+            }
+        })
+        .then(data => {
+            if (isValidAccount(data[0].password,password)) {
+                req.session.role = data[0].role
+                return res.send('thats right')
+            } else{
+                return res.redirect('/login?error=Email and Password is wrong')
+            }
+        })
+        .catch(err => {
+            res.redirect('/login?error=Email and Password is wrong')
+        })
+    }
+
+    static logOut(req, res) {
+        req.session.destroy(err => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/login')
+            }
+        })
+    }
+}
+
+module.exports = userController
