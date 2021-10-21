f4a179dfddbba2b60ec081fce03c84caedde5793
Khodhi Robbani <raihanrobbani12@gmail.com>
2021-10-21 23:13:39 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const loginRouter = require('./login')
 
 const router = express.Router()
 
+router.get('/', (req, res => {
+    return res.status(200).json({
+        halo: 'hai'
+    })
+}))
 router.use('/home', homeRouter)
 router.use('/user', loginRouter)
 
